aa39c3479c3478c05d741488253733d3522e1e6f
Prince Neres <prince84224960@gmail.com>
2022-04-19 15:05:46 -0300

    Add template perfis

--- experiencein/perfis/urls.py
+++ experiencein/perfis/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('perfis/<int:perfil_id>', views.exibir, name='exibir'),
 ]
--- experiencein/perfis/views.py
+++ experiencein/perfis/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def index(request): 
     return render(request, 'index.html')
+
+def exibir(request, perfil_id):
+    print('ID do perfil recebido: {}'.format(perfil_id))
+    return render(request, 'perfil.html')
