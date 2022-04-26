4efbba950b4e4e9803ca9a3840d89e080932b274
Prince Neres <prince84224960@gmail.com>
2022-04-26 14:43:50 -0300

    Add páginas dedicadas perfis

--- experiencein/perfis/views.py
+++ experiencein/perfis/views.py
@@ -2,15 +2,9 @@ from django.shortcuts import render
 from perfis.models import Perfil
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', { 'perfis' : Perfil.objects.all()})
 
 def exibir(request, perfil_id):
 
-    perfil = Perfil()
-
-    if perfil_id == 1:
-        perfil = Perfil('Fábio Henrique', 'fabio.oliveira@ifb.edu.br', '222222', 'IFB')
-    if perfil_id == 2:
-        perfil = Perfil('Elon Musk', 'elon.musk@tesla.com', '333333', 'Tesla') 
-
-    return render(request, 'perfil.html', {'perfil' : perfil})
+    perfil = Perfil.objects.get(id=perfil_id)
+    return render(request, 'perfil.html', { "perfil" : perfil})
