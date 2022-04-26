b9e1215a3b0cc8c5620303fea554420fbf0a899f
Prince Neres <prince84224960@gmail.com>
2022-04-26 11:29:06 -0300

    Add Páginas dinâmicas

--- experiencein/perfis/views.py
+++ experiencein/perfis/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render 
+from django.shortcuts import render
+from perfis.models import Perfil
 
-def index(request): 
+def index(request):
     return render(request, 'index.html')
 
 def exibir(request, perfil_id):
-    print('ID do perfil recebido: {}'.format(perfil_id))
-    return render(request, 'perfil.html')
+
+    perfil = Perfil()
+
+    if perfil_id == 1:
+        perfil = Perfil('Fábio Henrique', 'fabio.oliveira@ifb.edu.br', '222222', 'IFB')
+    if perfil_id == 2:
+        perfil = Perfil('Elon Musk', 'elon.musk@tesla.com', '333333', 'Tesla') 
+
+    return render(request, 'perfil.html', {'perfil' : perfil})
