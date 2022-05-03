021bb6a42bf46522889f09d2e3b34769e25dd102
Prince Neres <prince84224960@gmail.com>
2022-05-03 14:41:52 -0300

    Add convites a tela home

--- experiencein/perfis/views.py
+++ experiencein/perfis/views.py
@@ -2,13 +2,12 @@ from django.shortcuts import render
 from perfis.models import Perfil
 from django.shortcuts import redirect
 
-
 def index(request):
-    return render(request, 'index.html', { 'perfis' : Perfil.objects.all()})
+    return render(request, 'index.html', {'perfis' : Perfil.objects.all(), 'perfil_logado' : get_perfil_logado(request)})
 
 def exibir(request, perfil_id):
     perfil = Perfil.objects.get(id=perfil_id)
-    return render(request, 'perfil.html', { "perfil" : perfil})
+    return render(request, 'perfil.html', {'perfil' : perfil, 'perfil_logado' : get_perfil_logado(request)})
 
 def convidar(request, perfil_id):
     perfil_a_convidar = Perfil.objects.get(id=perfil_id)
